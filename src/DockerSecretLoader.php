b80ea5d5e1c2736a7e612f5327eddadc757f511f
pascalzoet <pascalzoet@users.noreply.github.com>
2021-09-22 16:29:08 +0200

    change to global function

--- src/DockerSecretLoader.php
+++ src/DockerSecretLoader.php
@@ -1,16 +1,11 @@
 <?php
 
-namespace DockerSecret;
-
-class DockerSecretLoader
+function docker_secret($name)
 {
-    public static function docker_secret($name)
-    {
-        return trim(file_get_contents('/run/secrets/' . $name));
-    }
+    return trim(file_get_contents('/run/secrets/' . $name));
+}
 
-    public static function docker_secret_callable($name)
-    {
-        return self::docker_secret($name);
-    }
+function docker_secret_callable($name)
+{
+    return docker_secret($name);
 }
