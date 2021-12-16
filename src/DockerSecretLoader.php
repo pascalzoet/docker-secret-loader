93ae5ae0b9cc70c70e54c0095627271816d179c6
pascalzoet <pascalzoet@users.noreply.github.com>
2021-12-16 12:43:17 +0100

    Update DockerSecretLoader.php

--- src/DockerSecretLoader.php
+++ src/DockerSecretLoader.php
@@ -2,7 +2,11 @@
 
 function docker_secret($name)
 {
-    return trim(file_get_contents('/run/secrets/' . $name));
+    try {
+       return trim(file_get_contents('/run/secrets/' . $name));
+    } catch(Exception $ex) {
+        return null;
+    }
 }
 
 function docker_secret_callable($name)
